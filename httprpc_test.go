67fd1cdfd2dae9c293e1b78896d213c3d2691b37
Michael Woolnough <michael.woolnough@gmail.com>
2024-08-16 17:23:08 +0100

    added additional test

--- httprpc_test.go
+++ httprpc_test.go
@@ -32,6 +32,10 @@ func TestRPC(t *testing.T) {
 			"{\"method\":\"RPCT.TrimSpace\",\"params\":[\" 123 \"],\"id\":0}\n{\"method\":\"RPCT.TrimSpace\",\"params\":[\" 123 \"],\"id\":0}",
 			"{\"id\":0,\"result\":\"123\",\"error\":null}\n{\"id\":0,\"result\":\"123\",\"error\":null}\n",
 		},
+		{
+			"{\"method\":\"RPCT.TrimSpace\",\"params\":[\" 123 ABC \"],\"id\":1}\n{\"method\":\"RPCT.TrimSpace\",\"params\":[\"\\tABC 123\\t\"],\"id\":2}\n",
+			"{\"id\":2,\"result\":\"ABC 123\",\"error\":null}\n{\"id\":1,\"result\":\"123 ABC\",\"error\":null}\n",
+		},
 	} {
 		resp, _ := srv.Client().Post(srv.URL+"/", "application/json", strings.NewReader(test.Input))
 		io.Copy(&buf, resp.Body)
